d6b22af3745b09cb506a9527caabdc507bee2845
slowdony <435574793@qq.com>
2017-08-24 00:14:56 +0800

    添加字典

--- SDSwift/SDDictionaryViewController.swift
+++ SDSwift/SDDictionaryViewController.swift
@@ -14,10 +14,69 @@ class SDDictionaryViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.title = "字典的练习"
         view.backgroundColor = UIColor.white
-        demo()
+        demo3()
         // Do any additional setup after loading the view.
     }
 
+    
+    //MARK: -字典的合并
+    func demo3() {
+        var dic = ["title":"小明","age":"18"]
+        var dic1 = ["title":"小郑","name":"21"]
+        
+//        dic += dic1 //不能像数组一样合并
+        /*
+         遍历dic1 ,依次设置
+         如果有相同的key ,修改key对应的value
+         如果没有就添加
+         */
+        for e in dic1 {
+            dic1[e.key] = dic[e.key]
+            print(dic1)
+        }
+    }
+    
+    //MARK: - 字典的遍历
+    
+    func demo2()  {
+        let dic = ["title":"小明","age":"18"]
+        
+        //元祖 key:string value:NSObject
+        
+        for e in dic {
+            print ("ekey:\(e.key) evalue:\(e.value)")
+        }
+        
+        /*
+        前者为key.后者为value
+         */
+        
+        for (key,value) in dic{
+            print ("\(key) \(value)")
+        }
+        
+    }
+    //MARK: - 字典的增删改
+    func demo1()  {
+        //var可变    let不可变
+        var dic = ["title":"小明","age":"18"]
+        print(dic)
+        
+        //新增字典
+        dic["name"] = "张晓明"
+        print (dic)
+        
+        //修改字典
+        dic["title"] = "小张"
+        print (dic)
+        
+        //删除字典
+        dic .removeValue(forKey: "age")
+        print (dic)
+        
+    
+    }
+    
     //MARK: - 字典的定义
     func demo() {
         let dic = ["title":"小明","age":"18"]
